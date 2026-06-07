452fcc74589753361026f588d816cc2c43953335
Davi Mariano <davi.mariano.r@gmail.com>
2026-06-07 15:32:33 -0300

    Primeira Classe e manipulando Objeto.

---
+++ src/Movie.java
@@ -0,0 +1,11 @@
+public class Movie {
+
+    String name;
+    int releaseDate;
+    boolean planIncluded;
+    int plays;
+    double review;
+    int totalReviews;
+    String genre;
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+
+    static void main() {
+        Movie movie1 = new Movie();
+        movie1.name = "Gigantes de Aço";
+        movie1.genre = "Ação";
+        movie1.releaseDate = 2011;
+        movie1.planIncluded = true;
+        movie1.review = 7.1;
+    }
+}
