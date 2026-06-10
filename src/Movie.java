957de5106cada747adfd0eb1a530547f624febcb
Davi Mariano <davi.mariano.r@gmail.com>
2026-06-10 12:48:52 -0300

    Criação e utilização de métodos.

--- src/Movie.java
+++ src/Movie.java
@@ -4,8 +4,29 @@ public class Movie {
     int releaseDate;
     boolean planIncluded;
     int plays;
-    double review;
-    int totalReviews;
+    double averageReview;
+    private double reviewAmount;
+    private int totalReviews;
     String genre;
+    String synopsis;
 
+    void displayInfo(){
+        System.out.println("Nome do Filme: " + name);
+        System.out.println("Data de Lançamento: " + releaseDate);
+        System.out.println("Gênero: " + genre);
+        System.out.println("Sinopse: " + synopsis);
+    }
+
+    void measuringReviews(double review){
+        reviewAmount += review;
+        totalReviews++;
+    }
+
+    double starGradeReview(){
+        return averageReview = (reviewAmount / totalReviews) / 2;
+    }
+
+    int getTotalReviews(){
+        return totalReviews;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Principal {
 
+
     static void main() {
+        Scanner reviewCatcher = new Scanner(System.in);
+
         Movie movie1 = new Movie();
         movie1.name = "Gigantes de Aço";
         movie1.genre = "Ação";
         movie1.releaseDate = 2011;
+        movie1.synopsis = """
+                Em um futuro próximo onde o boxe robótico é um
+                esporte de alto nível, um ex-boxeador em
+                dificuldades sente que encontrou um campeão em
+                um robô descartado.
+                """;
         movie1.planIncluded = true;
-        movie1.review = 7.1;
+
+        movie1.displayInfo();
+
+        movie1.measuringReviews(7.6);
+        movie1.measuringReviews(9.8);
+        movie1.measuringReviews(7.2);
+        movie1.measuringReviews(8);
+        System.out.println("Que nota você dá para este filme?");
+        movie1.measuringReviews(reviewCatcher.nextDouble());
+
+        System.out.println("Total de Avaliações: " + movie1.getTotalReviews());
+        System.out.println("Estrelas: " + movie1.starGradeReview());
     }
 }
